9c6ba69dee595191f39be78fac90e2c69d2800ea
Lennart Grahl <lennart.grahl@gmail.com>
2019-02-24 12:27:34 +0100

    Remove utils that are not needed by both rawrtcdc and rawrtc

--- include/rawrtcc/utils.h
+++ include/rawrtcc/utils.h
@@ -27,14 +27,6 @@ enum rawrtc_code rawrtc_strdup(
     char const * const source
 );
 
-/// Print a formatted string to a buffer.
-enum rawrtc_code rawrtc_snprintf(
-    char* const destinationp, // de-referenced
-    size_t const size,
-    char* const formatter,
-    ...
-);
-
 /// Print a formatted string to a dynamically allocated buffer.
 /// `*destinationp` must be unreferenced.
 enum rawrtc_code rawrtc_sdprintf(
